df67565978ee2fe2a3a44275af1add01eb5a31eb
Rafe <rsymonds@umich.edu>
2024-01-30 15:23:50 -0500

    python test add

--- test_examples.py
+++ test_examples.py
@@ -1,1 +1,7 @@
-import ex01a
+import pythontest
+
+def test_example():
+    assert pythontest.add(5,10) == 15
+    assert pythontest.add(2,5) == 7
+    assert pythontest.add(8,10) == 18
+    assert pythontest.add(-6,1) == -5
