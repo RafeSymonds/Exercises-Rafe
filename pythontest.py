0572a2c0474e773f511d2fbd5765246480c9b706
Rafe <rsymonds@umich.edu>
2024-01-30 15:33:17 -0500

    mean median mode

--- pythontest.py
+++ pythontest.py
@@ -1,2 +1,20 @@
 def add(num1, num2):
     return num1 + num2
+
+
+# return mean of numbers
+# reutnr 0 if numbers is empty
+def mean(numbers):
+    return sum(numbers) / len(numbers)
+
+def mode(numbers):
+    numOccurrences = dict()
+    for num in numbers:
+        numOccurrences[num] += 1
+
+def median(numbers):
+    numbers.sort()
+    if(len(numbers) % 2 == 0):
+        return (numbers[len(numbers) / 2] + numbers[len(numbers) / 2]) / 2
+    else:
+        return numbers[len(numbers) / 2]
--- test_examples.py
+++ test_examples.py
@@ -1,7 +1,21 @@
 import pythontest
 
-def test_example():
+def test_add():
     assert pythontest.add(5,10) == 15
     assert pythontest.add(2,5) == 7
     assert pythontest.add(8,10) == 18
     assert pythontest.add(-6,1) == -5
+
+def test_mean():
+    assert pythontest.mean([5,4,3,2,1]) == 3
+    assert pythontest.mean([1]) == 1
+    assert pythontest.mean([]) == 0
+    
+def test_mode():
+    assert pythontest.mode([5,4,3,2,1, 6, 1]) == 1
+    assert pythontest.mode([1]) == 1
+
+        
+def test_median():
+    assert pythontest.median([5,4,3,2,1]) == 3
+    assert pythontest.median([1]) == 1
