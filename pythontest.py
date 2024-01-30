803e8e38e75379caacf2a5f6ba53a46e33bca1d6
Rafe <rsymonds@umich.edu>
2024-01-30 15:43:41 -0500

    working code

--- pythontest.py
+++ pythontest.py
@@ -5,16 +5,21 @@ def add(num1, num2):
 # return mean of numbers
 # reutnr 0 if numbers is empty
 def mean(numbers):
+    if(len(numbers) == 0): return 0
     return sum(numbers) / len(numbers)
 
 def mode(numbers):
     numOccurrences = dict()
     for num in numbers:
-        numOccurrences[num] += 1
+        if(num in numOccurrences):
+            numOccurrences[num] += 1
+        else:
+            numOccurrences[num] = 1
+    return max(numOccurrences, key=numOccurrences.get)
 
 def median(numbers):
     numbers.sort()
     if(len(numbers) % 2 == 0):
-        return (numbers[len(numbers) / 2] + numbers[len(numbers) / 2]) / 2
+        return (numbers[int(len(numbers) / 2)] + numbers[int(len(numbers) / 2)]) / 2
     else:
-        return numbers[len(numbers) / 2]
+        return numbers[int(len(numbers) / 2)]
