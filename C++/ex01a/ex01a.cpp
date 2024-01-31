d72ec90a60837a0e1578c22ae126d90b93243d43
Rafe <rsymonds@umich.edu>
2024-01-31 12:35:11 -0500

    broken C++

--- C++/ex01a/ex01a.cpp
+++ C++/ex01a/ex01a.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     string word;
     while (cin >> word) {
-        cout << word << '\n';
+        cout << word << ' ';
     }
     // std::cout << 5 << std::endl;
     return 0;
