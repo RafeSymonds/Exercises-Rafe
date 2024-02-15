229023e9a45dd606192335af8b1dd9818e259cf4
Rafe <rsymonds@umich.edu>
2024-02-15 16:02:59 -0500

    ex06

--- C++/ex06/ex06.cpp
+++ C++/ex06/ex06.cpp
@@ -5,27 +5,5 @@
 using namespace std;
 
 int main() {
-    string s1;
-    string s2;
-    cin >> s1 >> s2;
-
-    if (s1.size() != s2.size()) {
-        cout << "False\n";
-        return 0;
-    }
-
-    vector<int> charsCount(26, 0);
-
-    for (size_t i = 0; i < s1.size(); ++i) {
-        ++charsCount[s1[i] - 'a'];
-        --charsCount[s2[i] - 'a'];
-    }
-    for (size_t i = 0; i < charsCount.size(); ++i) {
-        if (charsCount[i] != 0) {
-            cout << "False\n";
-            return 0;
-        }
-    }
-    cout << "True\n";
     return 0;
 }
