d3c9eaa221b0ed30a085cdac369660b03704fb84
Rafe <rsymonds@umich.edu>
2024-02-15 20:09:36 -0500

    ex07 no code

--- C++/ex07/ex07.cpp
+++ C++/ex07/ex07.cpp
@@ -3,19 +3,5 @@
 using namespace std;
 
 int main() {
-    int num = 0;
-    cin >> num;
-
-    if (num <= 1) {
-        cout << "False\n";
-        return 0;
-    }
-    for (int i = 2; i < num; ++i) {
-        if (num % i == 0) {
-            cout << "False\n";
-            return 0;
-        }
-    }
-    cout << "True\n";
     return 0;
 }
