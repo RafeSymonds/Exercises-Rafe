3490d0197d7a0599f6aa268b031812b3bff839a3
Rafe <rsymonds@umich.edu>
2024-02-19 08:28:46 -0500

    ex01

--- C++/ex01/ex01.cpp
+++ C++/ex01/ex01.cpp
@@ -1,14 +1,11 @@
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main() {
-    string word;
-    while (cin >> word) {
-        cout << word << ' ';
+    for (int i = 5; i <= 10; ++i) {
+        cout << i << '\n';
     }
-    // std::cout << 5 << std::endl;
     return 0;
 }
