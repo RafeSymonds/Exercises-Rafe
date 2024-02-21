c6ca00e5fa9af26bf079bd589e83cfedeb41f311
Rafe <rsymonds@umich.edu>
2024-02-21 14:54:43 -0500

    python ex01 new

--- Python/ex01/ex01.py
+++ Python/ex01/ex01.py
@@ -2,20 +2,12 @@ import math
 import statistics
 
 def fibonacciCalculator(index: int):
-    # counter = 3
-    # values = [0, 1, 1]
-    # while(counter < index + 1):
-    #     values.append(values[counter-1] + values[counter-2])
-    #     counter += 1
-    # return values[index]
-    
-
-    if(index == 0):
-        return 0
-    if(index == 1 or index == 2):
-        return 1
-    return fibonacciCalculator(index -1) + fibonacciCalculator(index - 2)
-
+    counter = 3
+    values = [0, 1, 1]
+    while(counter < index + 1):
+        values.append(values[counter-1] + values[counter-2])
+        counter += 1
+    return values[index]
 
 # add your own tests below and run with "python3 Python/ex01/ex01.py"
 # no output means your tests passed
